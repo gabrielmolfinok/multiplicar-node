d21f7e77aa789ed35a560a02f8283df47ac4a52b
Gabriel Molfino <gabrielmolfinok@gmail.com>
2018-07-09 22:16:32 -0300

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,45 @@
+// Requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+  console.log('==========================');
+  console.log(`Tabla del ${base}`.green);
+  console.log('==========================');
+
+  for (var i = 1; i <= limite; i++) {
+    console.log(`${base} * ${i} = ${base * i}`);
+  }
+
+}
+
+let crearArchivo = (base, limite = 10) => {
+    return new Promise ((resolve, reject) => {
+
+      if (!Number(base)) {
+        reject(`El valor introducido (${base}) no es un numero`);
+        return;
+      }
+
+      let data = '';
+
+      for (var i = 1; i <= limite; i++) {
+        data += `${ base } * ${ i } = ${ base * i }\n`;
+      }
+
+
+      fs.writeFile(`tablas/tabla-del-${base}-al-${limite}.txt`, data, (err) => {
+        if (err)
+          reject(err)
+        else
+          resolve(`tabla-del-${base}-al-${limite}.txt`);
+      });
+
+    });
+}
+
+module.exports = {
+  crearArchivo,
+  listarTabla
+}
